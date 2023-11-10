75fcf4f6c8d73169a369d80e9d21b31666f2267a
JonathanKoerber <jonathankoerber@gmail.com>
2023-11-10 15:06:49 -0800

    xRevert "adding auth"
    This reverts commit 4737daa43161fb0591e80c19fdc090c14bbb251e.

--- assignment_backend/settings.py
+++ assignment_backend/settings.py
@@ -40,16 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "core",
     "core.user",
-    "core.auth",
 ]
-
-REST_FRAMEWORK = {
-   'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework_simplejwt.authentication.JWTAuthentication',
-   ),
-   'DEFAULT_FILTER_BACKENDS':
-     ['django_filters.rest_framework.DjangoFilterBackend'],
-}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
