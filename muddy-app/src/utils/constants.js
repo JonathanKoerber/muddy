1e17e3dbe2e858740bcfe9b2c37e1b5e86801438
JonathanKoerber <jonathankoerber@gmail.com>
2023-11-15 21:15:19 -0800

    getting djano to run HTTPS

--- muddy-app/src/utils/constants.js
+++ muddy-app/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const requestBase = "https://packtpublishing.github.io/Simplifying-State-Management-in-React-Native";
 export const TOCKEN_KEY = "jwt-token";
-export const API_URL_BASE = "https://127.0.0.1:8000/api/";
+export const LOGIN_URL_BASE = "https://localhost:8000/api/auth/login/";
